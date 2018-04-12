0d067d316ef25020a5c60a61e03589ca6ad2c29d
Paul Franco <paulfranco@me.com>
2018-04-12 12:51:54 -0700

    completed app

--- app/src/main/java/co/paulfran/paulfranco/justjava/MainActivity.java
+++ app/src/main/java/co/paulfran/paulfranco/justjava/MainActivity.java
@@ -30,9 +30,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        //int quantity = 2;
-        //display(quantity);
-        displayPrice(quantity * 5);
+        int price = quantity * 5;
+        String priceMessage = "Total: $";
+        displayMessage(priceMessage + price +" \n Thank you");
+        //displayPrice(quantity * 5);
     }
 
     /**
@@ -48,5 +49,12 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
